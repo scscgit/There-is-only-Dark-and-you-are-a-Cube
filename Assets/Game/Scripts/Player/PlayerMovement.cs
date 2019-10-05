7823bd798a0ce878e7aca963435d7b71d7de2dbc
Jozef Černický <jozef.cernicky@student.tuke.sk>
2019-10-05 18:20:05 +0200

    movement stuff

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,7 @@ namespace Game.Scripts.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        public float movementSpeed = .20f;
+        public float movementSpeed = 200f;
 
         private Vector3 _movement;
         private Rigidbody _rigidbody;
@@ -22,9 +22,44 @@ namespace Game.Scripts.Player
 
         void FixedUpdate()
         {
-            _rigidbody.AddTorque(new Vector3(_movement.z, 0, 0) * 1000 * movementSpeed, ForceMode.VelocityChange);
-            _rigidbody.AddTorque(new Vector3(0, 0, _movement.x) * 1000 * (-movementSpeed), ForceMode.VelocityChange);
-            //_rigidbody.MovePosition(transform.position + new Vector3(_movement.x * movementSpeed, 0, 0));
+            if (Mathf.Abs(_movement.z) > 0)
+            {
+                // Don't move both directions at once, and don't start to rotate until the other directions stops
+                if (Mathf.Abs(_movement.x) > 0)
+                {
+                    BlockRotation();
+                }
+
+                // Rotate forward/backward
+                _rigidbody.constraints |= RigidbodyConstraints.FreezePositionX;
+                _rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+
+                _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationZ;
+                _rigidbody.constraints &= ~RigidbodyConstraints.FreezeRotationX;
+
+                _rigidbody.AddTorque(new Vector3(_movement.z, 0, 0) * movementSpeed, ForceMode.VelocityChange);
+            }
+            else if (Mathf.Abs(_movement.x) > 0)
+            {
+                // Rotate left/right
+                _rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionX;
+                _rigidbody.constraints |= RigidbodyConstraints.FreezePositionZ;
+
+                _rigidbody.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+                _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationX;
+
+                _rigidbody.AddTorque(new Vector3(0, 0, -_movement.x) * movementSpeed, ForceMode.VelocityChange);
+            }
+            else
+            {
+                BlockRotation();
+            }
+        }
+
+        void BlockRotation()
+        {
+            _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationZ;
+            _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationX;
         }
     }
 }
