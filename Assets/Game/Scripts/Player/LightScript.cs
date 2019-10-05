fea4e9fc9a6da85bc81c7f0ec44cc6e742992ed7
Jozef Černický <jozef.cernicky@student.tuke.sk>
2019-10-05 14:50:53 +0200

    ta dal som tam skript na ovladanie svetla

---
+++ Assets/Game/Scripts/Player/LightScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private float lightIntensity;
+    private float lightDec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightIntensity = 100;
+        lightDec = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightIntensity -= lightDec;
+    }
+}
