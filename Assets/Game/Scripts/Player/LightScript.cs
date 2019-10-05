66a229de234d52ee2b201e425349fa3c89b05979
Jozef Černický <jozef.cernicky@student.tuke.sk>
2019-10-05 15:25:31 +0200

    LightScript - added actual light

--- Assets/Game/Scripts/Player/LightScript.cs
+++ Assets/Game/Scripts/Player/LightScript.cs
@@ -6,10 +6,24 @@ public class LightScript : MonoBehaviour
 {
     private float lightIntensity { get; set; }
     public float lightDec;
+    private Light lightComp;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // Make a game object
+        GameObject lightGameObject = new GameObject("The Light");
+
+        // Add the light component
+        lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = Color.white;
+        lightComp.intensity = lightIntensity/10;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = new Vector3(0, 5, 0);
         lightIntensity = 100;
         lightDec = 0.1f;
     }
@@ -19,5 +33,6 @@ public class LightScript : MonoBehaviour
     {
         if (lightIntensity>0)
             lightIntensity -= lightDec;
+        lightComp.intensity = lightIntensity/10;
     }
 }
