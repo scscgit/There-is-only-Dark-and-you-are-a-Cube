fc66b8a47cef5f2db7ecd10047a9035281de8138
scsc <scscgit@users.noreply.github.com>
2019-10-05 19:37:23 +0200

    Implemented a true cube rotation movement; made FollowPlayer automatic

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,12 @@ namespace Game.Scripts.Player
     public class PlayerMovement : MonoBehaviour
     {
         public float movementSpeed = 200f;
+        public float rotationDegreesToAllowDirectionSwitch = 15f;
+        public float slerpRotationNormalizationIdle = .25f;
 
         private Vector3 _movement;
         private Rigidbody _rigidbody;
+        private bool _horizontalMovement;
 
         void Awake()
         {
@@ -16,8 +19,8 @@ namespace Game.Scripts.Player
 
         void Update()
         {
-            _movement.x = Input.GetAxis("Horizontal");
-            _movement.z = Input.GetAxis("Vertical");
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.z = Input.GetAxisRaw("Vertical");
         }
 
         void FixedUpdate()
@@ -28,7 +31,16 @@ namespace Game.Scripts.Player
                 if (Mathf.Abs(_movement.x) > 0)
                 {
                     BlockRotation();
+                    return;
                 }
+
+                // If the previous movement was horizontal, make sure the cube stands on a side
+                if (_horizontalMovement && BlockRotation())
+                {
+                    return;
+                }
+
+                _horizontalMovement = false;
 
                 // Rotate forward/backward
                 _rigidbody.constraints |= RigidbodyConstraints.FreezePositionX;
@@ -41,6 +53,14 @@ namespace Game.Scripts.Player
             }
             else if (Mathf.Abs(_movement.x) > 0)
             {
+                // If the previous movement was vertical, make sure the cube stands on a side
+                if (!_horizontalMovement && BlockRotation())
+                {
+                    return;
+                }
+
+                _horizontalMovement = true;
+
                 // Rotate left/right
                 _rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionX;
                 _rigidbody.constraints |= RigidbodyConstraints.FreezePositionZ;
@@ -56,10 +76,43 @@ namespace Game.Scripts.Player
             }
         }
 
-        void BlockRotation()
+        /// <summary>
+        /// Normalizes the cube to stand on a side.
+        /// </summary>
+        /// <returns>true if there was a compensation rotation applied</returns>
+        bool BlockRotation()
         {
             _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationZ;
             _rigidbody.constraints |= RigidbodyConstraints.FreezeRotationX;
+
+            var slerpTarget = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.Euler(
+                    Mathf.Round(transform.rotation.eulerAngles.x / 90f) * 90,
+                    Mathf.Round(transform.rotation.eulerAngles.y / 90f) * 90,
+                    Mathf.Round(transform.rotation.eulerAngles.z / 90f) * 90
+                ),
+                slerpRotationNormalizationIdle
+            );
+            var differenceX = transform.rotation.eulerAngles.x % 90;
+            while (differenceX > 45)
+            {
+                differenceX -= 90;
+            }
+
+            var differenceY = transform.rotation.eulerAngles.z % 90;
+            while (differenceY > 45)
+            {
+                differenceY -= 90;
+            }
+
+            if (Mathf.Max(Mathf.Abs(differenceX), Mathf.Abs(differenceY)) > rotationDegreesToAllowDirectionSwitch)
+            {
+                _rigidbody.MoveRotation(slerpTarget);
+                return true;
+            }
+
+            return false;
         }
     }
 }
