06b9d3f1c718574c153062e7155239a149bc45ab
scsc <scscgit@users.noreply.github.com>
2019-10-07 23:47:17 +0200

    Added a Main Menu screen, and a logo

---
+++ Assets/Game/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Scripts
+{
+    public class MainMenu : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                StartGame();
+            }
+        }
+
+        public void StartGame()
+        {
+            try
+            {
+                SceneManager.LoadScene(AssetBundle.LoadFromFile("Assets/Game/Scene").GetAllScenePaths()[0]);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                try
+                {
+                    SceneManager.LoadScene("SimpleScene");
+                }
+                catch (Exception e2)
+                {
+                    Debug.LogException(e2);
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+    }
+}
